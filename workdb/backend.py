c3c9714c7c2ef8901b8da00ac2e65d82248e37dc
James Pestrak <james.w.pestrak@gmail.com>
2019-04-10 07:56:50 -0700

    Add functionality for writing records to database.

--- workdb/backend.py
+++ workdb/backend.py
@@ -12,6 +12,29 @@ class Task(Model):
         database = db
 
 
+tasks = [
+    {'username': 'James',
+     'taskname': 'wake up',
+     'duration': 10},
+    {'username': 'Georgia',
+     'taskname': 'eat breakfast',
+     'duration': 5},
+]
+
+
+def add_tasks():
+    for task in tasks:
+        try:
+            Task.create(username=task['username'],
+                        taskname=task['taskname'],
+                        duration=task['duration'])
+        except IntegrityError:
+            task_record = Task.get(username=task['username'])
+            task_record.duration = task['duration']
+            task_record.save()
+
+
 if __name__ == '__main__':
     db.connect()
     db.create_tables([Task], safe=True)
+    add_tasks()
