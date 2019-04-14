471ed3660efb38d539282e8c29da1bc27ddb244e
James Pestrak <james.w.pestrak@gmail.com>
2019-04-14 16:03:49 -0700

    Satisfy user story "Enter a new work log."

--- workdb/work_log.py
+++ workdb/work_log.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from collections import OrderedDict
+import sys
 
 from peewee import *
 
@@ -29,30 +30,32 @@ def menu_loop():
             print("{}) {}".format(key, value.__doc__))
         choice = input('Action: ').lower().strip()
 
-    if choice in menu:
-        menu[choice]()
+        if choice in menu:
+            menu[choice]()
 
-# As a user of the script,
-
-#  I should be able to choose whether to add a new entry or lookup previous entries.
-
-#  if I choose to enter a new work log, I should be able to provide my name, a task name, a number of minutes spent working on it, and any additional notes I want to record.
-
-#  if I choose to find a previous entry, I should be presented with four options: find by employee, find by date, find by time spent, find by search term.
-
-#  if finding by employee, I should be presented with a list of employees with entries and be able to choose one to see entries from.
-
-#  if finding by employee, I should be allowed to enter employee name and then be presented with entries with that employee as their creator.
-
-#  if finding by date, I should be presented with a list of dates with entries and be able to choose one to see entries from.
-
-#  if finding by time spent, I should be allowed to enter the amount of time spent on the project and then be presented with entries containing that amount of time spent.
-
-#  if finding by a search term, I should be allowed to enter a string and then be presented with entries containing that string in the task name or notes.
-
-# As a fellow developer, I should find at least 50% of the code covered by tests. I would use coverage.py to validate this amount of coverage.
 def add_entry():
-    pass
+    """Enter a new work log."""
+    print("Enter your username. Press ctrl+d when finished.")
+    data_username = sys.stdin.read().strip()
+
+    print("Enter a name for your task. Press ctrl+d when finished.")
+    data_taskname = sys.stdin.read().strip()
+
+    print("Enter the number of minutes spent on task. Press ctrl+d when finished.")
+    data_duration = sys.stdin.read().strip()
+
+    print("Enter additional notes. Press ctrl+d when finished.")
+    data_notes = sys.stdin.read().strip()
+
+    if (data_username and data_taskname and data_duration):
+        if input("Save work log? [Yn] ") != "n":
+            Task.create(
+                username = data_username,
+                taskname = data_taskname,
+                duration = data_duration,
+                notes    = data_notes
+            )
+            print("Work log saved successfully!")
 
 def view_entries():
     pass
